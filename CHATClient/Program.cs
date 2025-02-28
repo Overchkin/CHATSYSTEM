3a1d110de7253a6460a98ed6abb907bf02739599
Israël TODOME <tdonanon@outlook.fr>
2025-02-28 17:59:50 -0500

    Ajoutez des fichiers projet.

---
+++ CHATClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CHATClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            var endpoint = new IPEndPoint(
+                IPAddress.Loopback,
+                2345);
+        }
+        Socket.Connect(EndPoint);
+
+        while(true)
+            {
+                string message = Console.ReadLine();
+                If(message == "q")
+                {
+                    break;
+                }
+                var buffer = Encoding.UTF8.GetBytes(message);
+                Socket.Send(buffer);
+            }
+    }
+}
---
+++ CHATServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.WebSockets;
+using NetMQ.Sockets;
+using System.Text;
+
+namespace CHATServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Any, 2345
+                    );
+                socket.Bind(endPoint);
+                socket.Listen(10);
+                socket.Accept();
+                if (ClientSocket.RemoteEndPoint != null)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Client connecté depuis" + ClientSocket.RemoteEndPoint.ToString());
+                        byte[] buffer = new byte[128];
+                        int nb = ClientSocket.Receive(buffer);
+                        Console.WriteLine("Message reçu: " + Encoding.UTF8.GetString(buffer, 0, nb));
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Une erreur s'est produite sur le serveur");
+            }
+        }
+    }
+}
